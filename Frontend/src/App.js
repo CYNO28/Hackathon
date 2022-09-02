22e80f02aa594afb7e233f55210a5c2d80a0fcf2
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-09-02 16:36:14 +0530

    1

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './Pages/Login';
+import { Routes, Route } from "react-router-dom";
+import Sign from './Pages/Sign';
 function App() {
   return (
-    <div className="App">
+  
+   <>
+   <Routes>
+    <Route path={'/login'} element={<Login/>}/>
+    <Route path={'/signup'} element={<Sign/>}/>
+
+   </Routes>
+ 
+   </>
    
-    </div>
   );
 }
 
